98c3bf5cc54d097a474627519550eb1dd5d46a90
rafeh398 <rafeh398@gmail.com>
2025-01-15 12:10:40 +0000

    commit

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,17 @@
+const accountId=144533
+let accountEmail="rafeh398@gmail.com"
+var accountPassword="1122"
+accountCity="Jhelum"
+
+// accountId=2   not allowed
+
+accountEmail="rafeh771@gmail.com"
+accountPassword="3344"
+accountCity="dina"
+
+let accountState;
+
+// prefer not to use var because of issue in block scope and functional scope
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState]) ;
+
