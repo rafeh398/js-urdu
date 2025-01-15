3d63b2421cb83f0ab46e608129fdb68b6a1193ea
rafeh398 <rafeh398@gmail.com>
2025-01-15 12:39:47 +0000

    datatype

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,23 @@
+"use strict";  // this treat js code as newer  version
+
+// alert(3+3)  // this is nodejs not browser
+
+let name="rafeh"
+let age=23
+let isLoggedIn=false
+
+// number => 2 to the power 53
+//big int
+
+
+// string => ""
+//boolean => true / false
+//null => standalone value => representation of empty value but defined => null is object when it is wriiten after typeof
+// undefined => when value is not assigned
+//symbol => unique
+
+console.log(typeof age); // find data type of variable name
+
+
+
+//object
