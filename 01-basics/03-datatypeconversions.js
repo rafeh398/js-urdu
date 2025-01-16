40e4395db7efde24bd0245582729096fe7392e76
rafeh398 <rafeh398@gmail.com>
2025-01-16 07:51:06 +0000

    DATATYPES

--- 01-basics/03-datatypeconversions.js
+++ 01-basics/03-datatypeconversions.js
@@ -26,3 +26,76 @@ let stringNumber= String(someNumber)
 console.log(typeof stringNumber);
 
 // number is converted to string
+
+
+// ********OPERATIONS**********
+
+let value=3
+let negValue=-value
+console.log(negValue);
+
+// + console.log(2+2)
+//-
+// *
+// 4/2=2 divide
+// 2**3  2 raise to the power 3
+// 4%2 remainder
+
+//  strind addition
+  
+let str1="hello"
+let str2= " rafeh"
+let str3=str1 + str2
+console.log(str3);  // answer is simply HELLO RAFEH
+
+console.log("1"+2);
+console.log(1+"2");    // both answers are 12
+
+
+
+console.log("1"+2+2);   // answer is 122
+console.log(1+2 +"2");  //answer is 32        // in both cases JS will take 1st number or string priority
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);   //answer is 101
+
+// ************MDN doc************* increment prefix and postpix
+ /* let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4" */
+
+
+//*********comparisons*******
+//  console.log(2>1);
+//  (2<1);
+//  (2<1);
+//  (2>=1);
+//  (2==1);
+//  (2!=1); 
+
+
+
+// ******** comaparions of different types
+// console.log(null> 0)  // unpredictable result  //AVoid these type of comparisons
+// === triple equal strictly checks the values 
+   
+
+
+
+
+
+
+
+
+
+
+
